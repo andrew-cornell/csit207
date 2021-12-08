5d571574410f8f8d1a1e8108f89e4db672f7fc6f
itrustu0 <64800395+itrustu0@users.noreply.github.com>
2021-12-08 10:54:07 -0500

    Add files via upload

---
+++ csit207/ArrToXML.php
@@ -0,0 +1,32 @@
+<?php
+$characters = array(
+    array( "name" => "Peter Parker", "email" => "peterparker@mail.com", ),
+    array( "name" => "Clark Kent", "email" => "clarkkent@mail.com", ),
+    array( "name" => "Harry Potter", "email" => "harrypotter@mail.com", )
+);
+array_push($characters, array("name" => "Zeus", "email" => "zeus@olympus.com", ));
+array_push($characters, array("name" => "Tony Stark", "email" => "ironman@avengers.com", ));
+
+echo "<pre>";
+print_r($characters);
+function array_to_xml( $data, &$xml_data ) {
+    foreach( $data as $key => $value ) {
+        if( is_array($value) ) {
+            if( is_numeric($key) ){
+                $key = 'item'.$key; //dealing with <0/>..<n/> issues
+            }
+            $next = $xml_data->addChild($key);
+            array_to_xml($value, $next);
+        } else {
+            $xml_data->addChild("$key",htmlspecialchars("$value"));
+        }
+    }
+}
+
+$xmlfile = new SimpleXMLElement('<character/>');
+
+array_to_xml($characters,$xmlfile);
+
+$xmlfile->asXML('characters.xml');
+
+?>
